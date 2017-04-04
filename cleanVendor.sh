59edba60747c33a457db34c93512b203bda41c96
RJ Catalano <rj@monax.io>
2017-04-04 15:29:34 -0500

    clean vendor script file
    Signed-off-by: RJ Catalano <rj@monax.io>

--- cleanVendor.sh
+++ cleanVendor.sh
@@ -2,6 +2,6 @@
 
 cp -r vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1 .
 go get -u github.com/sgotti/glide-vc
-glide vc
+glide vc --use-lock-file
 cp -r ./libsecp256k1 vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/
 rm -rf libsecp256k1
